acba317e9c6ce8d32f662cfbe1d8a807fc16921b
kudy <bayardkudy1@gmail.com>
2025-07-11 22:22:20 +0100

    Create Account constructor

--- BankAccount.cpp
+++ BankAccount.cpp
@@ -1,1 +1,34 @@
 #include <iostream>
+
+using namespace std;
+
+class Account {
+private:
+    string AccountNumber;
+    string AccountName;
+    double Balance;
+    string Password;
+    string Phone;
+    string Address;
+
+public:
+    Account(string accNum, string accName, double balance,
+            string password, string phone, string address){
+        if (balance < 0)
+            throw invalid_argument("Initial balance is invalid.");
+
+        AccountNumber = accNum;
+        AccountName = accName;
+        Balance = balance;
+        Password = password;
+        Phone = phone;
+        Address = address;
+
+    }
+
+};
+
+
+int main(){
+
+}
